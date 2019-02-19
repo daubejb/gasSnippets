6d9e019b2bad3bb2cb05c8ddb8d79d9847bd24ca
Jeffrey B. Daube <daubejb@gmail.com>
2019-02-19 18:01:08 +0000

    add getLatestFormResponse code snippet

--- Code.js
+++ Code.js
@@ -1,5 +1,10 @@
+/*
+ * Get the most recent form response object from a Google Form.
+ * To learn more about what to do with the formResponse, see:
+ * https://developers.google.com/apps-script/reference/forms/form-response
+ */
 function getLatestFormResponse() {
     var form = FormApp.openById('1H9sq4Z6m1a7hk-UAktQ_k9zLdkY-HtnlAHolWsLmdeQ');
-    var latestResponse = form.getResponses().slice(-1)[0];
-    Logger.log(latestResponse);
+    var latestFormResponse = form.getResponses().slice(-1)[0];
+    Logger.log(latestFormResponse);
 }

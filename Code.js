306d8e0f5db3cc35b18180940dc4c434c388bf8b
Jeffrey B. Daube <daubejb@gmail.com>
2019-02-19 18:17:53 +0000

    add get request to RESTful API

--- Code.js
+++ Code.js
@@ -8,3 +8,14 @@ function getLatestFormResponse() {
     var latestFormResponse = form.getResponses().slice(-1)[0];
     Logger.log(latestFormResponse);
 }
+
+/*
+ * Get request to a basic RESTful API
+ * The URL Fetch service can issue HTTP and HTTPS requests and receive repsonses
+ * https://developers.google.com/apps-script/reference/url-fetch/url-fetch-app#fetch(String)
+ */
+function getRequestToApi() {
+    var numbersApi = 'http://numbersapi.com/81';
+    var response = UrlFetchApp.fetch(numbersApi).getContentText();
+    Logger.log(response);
+}
